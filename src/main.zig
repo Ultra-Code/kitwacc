c770a60e18a83077c17f27434319073964deb05e
Ultra-Code <mega.alpha100@gmail.com>
2021-12-04 04:24:28 +0000

    Add TokenStream struct for managing input stream
    move argv lenght handling to fn main and use reportError
    for handling errors in matching tokens to terminals

--- src/main.zig
+++ src/main.zig
@@ -3,19 +3,48 @@ const cwd = std.fs.cwd();
 const tokenizers = @import("tokenizer.zig");
 const Tokenizer = tokenizers.Tokenizer;
 
-fn compileInt(allocator: *std.mem.Allocator, args: []const []const u8) !void {
-    const program_name = args[0];
-    std.log.info("argv is {s}", .{args});
-    if (args.len != 2) {
+const TokenStream = struct {
+    input: []const []const u8,
+
+    /// The caller must free the returned slice
+    fn init(allocator: *std.mem.Allocator) !TokenStream {
+        const args = try std.process.argsAlloc(allocator);
+        return TokenStream{ .input = args };
+    }
+};
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer if (gpa.deinit()) {
+        std.log.err("Memory Leaks Deteckted", .{});
+    };
+    var gpa_allocator = &gpa.allocator;
+    var arena = std.heap.ArenaAllocator.init(gpa_allocator);
+    defer arena.deinit();
+    var allocator = &arena.allocator;
+
+    const args = try TokenStream.init(allocator);
+
+    const program_name = args.input[0];
+    const stream = args.input[1];
+    std.log.info("argv is {s}", .{stream});
+    if (args.input.len < 2) {
         std.log.err("{s} has invalid number of arguments.Expected 1 found 0", .{program_name});
+        std.log.err("{s} call kitwacc with 1 argument", .{program_name});
         return error.InvalidNumberOfArguments;
     }
+    compileInt(allocator, stream) catch |err|
+        switch (err) {
+        else => std.log.err("{s} error occured", .{@errorName(err)}),
+    };
+}
 
+fn compileInt(allocator: *std.mem.Allocator, stream: []const u8) !void {
     const output = try std.fs.cwd().createFile("test/output.s", .{});
     defer output.close();
 
-    var tokenizer = Tokenizer.init(allocator);
-    var token = try tokenizer.tokenize(args[1]);
+    var tokenizer = Tokenizer.init(allocator, stream);
+    var token = try tokenizer.tokenize();
 
     const space = " ";
     try output.writer().print(
@@ -30,19 +59,19 @@ fn compileInt(allocator: *std.mem.Allocator, args: []const []const u8) !void {
 
     // ... followed by either `+ <number>` or `- <number>`.
     while (token.kind != .TK_EOF) : (token = tokenizer.nextToken()) {
-        if (Tokenizer.match(token, "+")) {
-            token = tokenizer.nextToken();
-            try output.writer().print("{s:>8}add ${d}, %rax\n", .{ space, token.value });
-            continue;
-        }
-        if (Tokenizer.match(token, "-")) {
-            token = tokenizer.nextToken();
-            try output.writer().print("{s:>8}sub ${d}, %rax\n", .{ space, token.value });
+        if (tokenizer.match(token, "+")) |next_token| {
+            const number = try tokenizer.getNumber();
+            try output.writer().print("{s:>8}add ${d}, %rax\n", .{ space, number });
             continue;
         }
 
-        tokenizers.reportError(args[1], "^ expected an operator", token);
-        std.process.exit(1);
+        if (tokenizer.match(token, "-")) |next_token| {
+            const number = try tokenizer.getNumber();
+            try output.writer().print("{s:>8}sub ${d}, %rax\n", .{ space, number });
+            continue;
+        }
+
+        tokenizer.reportError("expected token {s} to match terminal + or -", .{token.lexeme});
     }
 
     try output.writer().print(
@@ -58,24 +87,4 @@ test "compileInt test" {
     var fixed_allocator = &fba.allocator;
     const exit_code = try std.ChildProcess.exec(.{ .allocator = fixed_allocator, .argv = &[_][]const u8{"./test/test-compiler"} });
     std.debug.print("{s}\n{s}", .{ exit_code.stderr, exit_code.stdout });
-}
-
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer if (gpa.deinit()) {
-        std.log.err("Memory Leaks Deteckted", .{});
-    };
-    var gpa_allocator = &gpa.allocator;
-    var arena = std.heap.ArenaAllocator.init(gpa_allocator);
-    defer arena.deinit();
-    var allocator = &arena.allocator;
-
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
-
-    compileInt(allocator, args) catch |err|
-        switch (err) {
-        error.InvalidNumberOfArguments => std.log.err("{s} call kitwacc with 1 argument", .{@errorName(err)}),
-        else => std.log.err("{s} error occured", .{@errorName(err)}),
-    };
 }
